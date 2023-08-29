9085c882d4604e81a9233c16e5f87d84e200daab
dreamescime <128262023+dreamescime@users.noreply.github.com>
2023-08-29 11:01:57 +0200

    Update Symboles.ino

--- Symboles/Symboles.ino
+++ Symboles/Symboles.ino
@@ -3,3 +3,11 @@
 #define PIXEL_PIN_Led_RGB         2
 #define PIXEL_COUNT_Led_RGB       1
 Adafruit_NeoPixel stripRGB(PIXEL_COUNT_Led_RGB, PIXEL_PIN_Led_RGB, NEO_GRB + NEO_KHZ800);
+
+
+#define BP_1          3
+#define BP_2          4
+#define BP_3          5
+#define BP_4          6
+
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
