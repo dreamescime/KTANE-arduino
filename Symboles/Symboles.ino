f3fc54948d5eeed399bdb68e967fe7ee81b124dc
dreamescime <128262023+dreamescime@users.noreply.github.com>
2023-08-29 14:12:24 +0200

    Update Symboles.ino

--- Symboles/Symboles.ino
+++ Symboles/Symboles.ino
@@ -11,3 +11,29 @@ Adafruit_NeoPixel stripRGB(PIXEL_COUNT_Led_RGB, PIXEL_PIN_Led_RGB, NEO_GRB + NEO
 #define BP_4          6
 
 U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+
+byte ligne = 0;
+byte colonne[7] = {0, 1, 2, 3, 4, 5, 6};
+const byte symbole[6][7] = {
+{125, 112, 126, 111, 107, 109, 121},
+{115, 125, 121, 123, 103, 109, 118},
+{101, 108, 123, 105, 114, 126, 103},
+{110, 119, 127, 107, 105, 118, 104},
+{122, 104, 127, 120, 119, 117, 102},
+{110, 115, 124, 113, 122, 116, 106}};
+
+byte symbole_1 = 0;
+
+void initialisation() {
+  randomSeed(millis());
+      for (int h = 0; h < 500; h++) { //mellange la premiere ligne
+        int i = random(0, 7); 
+        int j = random(0, 7);
+        int swap = colonne[i];
+        colonne[i] = colonne[j];
+        colonne[j] = swap;}
+  ligne = random(0, 6);
+
+  //afficher les symboles 
+  */en premier : symbole[ligne][colonne[1]]
+}
